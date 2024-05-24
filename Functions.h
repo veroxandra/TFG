7428d2a8ffcee9cc75737cb6b0925dde7c88ff55
Verónica Matos <148332830+veroxandra@users.noreply.github.com>
2024-05-24 13:22:08 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -8,15 +8,21 @@
 #ifndef FUNCTIONS_H
 #define	FUNCTIONS_H
 
-char calculate_safety_distance(float actual_distance){}
+int calculate_safety_distance(float actual_floor_distance, float safety_floor_distance){}
 
-float data_conversion(float data){}
+float data_conversion(unsigned int data){}
 
 void wait_servo_movement(){}
 
-void LiDAR(){}
+void move_servo(){}
 
-void ultrasonic(){}
+unsigned int LiDAR(){}
+
+unsigned int ultrasonic(){}
+
+unsigned int coordinates(){}
+
+unsigned int floor(){}
 
 #endif	/* FUNCTIONS_H */
 
